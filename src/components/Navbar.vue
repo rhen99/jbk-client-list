d4d14e78df4b6890d9fb89eb864f8ef83575600d
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-06-30 19:50:18 +0800

    working on form

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-container>
+      <b-navbar-brand>
+        Client List
+      </b-navbar-brand>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-form>
+    <h4 class="my-2">Search Clients</h4>
+    <b-form-group>
+      <b-form-select :options="options" v-model="selected"></b-form-select>
+    </b-form-group>
+    <b-form-group>
+      <b-form-input placeholder="Write Here..." />
+    </b-form-group>
+  </b-form>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      selected: "name",
+      options: [
+        {
+          text: "Search By Name",
+          value: "name",
+        },
+        {
+          text: "Search By Address",
+          value: "address",
+        },
+        {
+          text: "Search By Date",
+          value: "date",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
