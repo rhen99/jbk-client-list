d41695b1c2b98c0f71bbdbe4804f667abb2f98ca
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-07-02 18:57:31 +0800

    working on the addform modal

--- src/components/AddForm.vue
+++ src/components/AddForm.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="my-3">
-    <b-button variant="success">Add Client</b-button>
-    <b-modal></b-modal>
+    <b-button variant="success" @click="modalShow = !modalShow">Add Client</b-button>
+    <b-modal ref="addForm" v-model="modalShow" hide-footer></b-modal>
   </div>
 </template>
 
 <script>
   export default {
-    name: "AddForm"
+    name: "AddForm",
+    data() {
+      return {
+        modalShow: false
+      };
+    }
   };
 </script>
 
