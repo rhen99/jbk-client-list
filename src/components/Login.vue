ae5373ccdd84f6083e2602bdf8b2deff1227b3e9
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-07-17 19:14:05 +0800

    sign in page created

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,24 @@
 <template>
-  <b-container class="mt-3"> </b-container>
+  <b-container class="mt-5">
+    <b-form>
+      <b-form-group label="Password" label-for="password">
+        <b-form-input type="password" id="password"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" block variant="primary">Sign in</b-button>
+    </b-form>
+  </b-container>
 </template>
 
 <script>
-export default {
-  name: "Login",
-};
+  export default {
+    name: "Login",
+    data() {
+      return {
+        email: null,
+        password: null
+      };
+    }
+  };
 </script>
 
 <style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from "vue";
 import Router from "vue-router";
 import ClientList from "../components/ClientList";
+import Login from "../components/Login";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
       name: "client-list",
       component: ClientList,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
     },
   ],
 });
